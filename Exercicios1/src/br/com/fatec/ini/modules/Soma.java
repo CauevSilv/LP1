31480277b2544ffb71c6202531e793aa81ed91e8
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2023-08-16 20:33:42 -0300

    Exercícios da Tarefa "Teste de Código"
    Matéria LP1

---
+++ Exercicios1/src/br/com/fatec/ini/modules/Soma.java
@@ -0,0 +1,11 @@
+package br.com.fatec.ini.modules;
+
+public class Soma {
+    public void somar_real(double a, double b){
+        System.out.println("A soma é : " + (a+b));
+        }
+        public void somar_complexo(int a, int b,int c,int d){
+            System.out.println("Soma complexa: " + (a+b+c+d));
+        }
+    }
+
